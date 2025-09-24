f674a1e85c632707438cfa79e97393138a78ac80
Debbie To <sto@alumni.constructor.university>
2025-09-25 00:52:03 +0200

    Added new components

--- app.py
+++ app.py
@@ -28,13 +28,13 @@ st.set_page_config(
 )
 
 # Sidebar menu
-sidebar_items = ["Home", "Data", "Model", "Manuscripts"]#
+sidebar_items = ["Home", "Data", "Model", "Manuscripts", "Planktoomics"]
 
 with st.sidebar:
     selected = option_menu(
         None,                                       # Title at the top of sidebar
         sidebar_items,                              # Menu items
-        icons=["house", "graph-up", "activity", "file-text"],   # Matching icons
+        icons=["house", "graph-up", "activity", "file-text", "book"],   # Matching icons
         # menu_icon="cast",                         # Icon for the menu title
         default_index=1,                            # Which tab opens first
         # orientation='horizontal'
@@ -49,54 +49,71 @@ st.sidebar.markdown("---")  # horizontal line
 # Reasons for this site
 with st.sidebar.expander("More about this app ℹ️"):
     st.write(
-        "Scientific dashboard",
+        "This is a scientific dashboard to showcase the results of my PhD research "
+        "in ecological modeling. The dashboard aims to make complex graphs more "
+        "accessible to a broader audience.",
         styles={""}
         )
 # My info
-with st.sidebar.expander("More about me 👤"):
+with st.sidebar.expander("Something about me 👤"):
     st.write(
-        "I am ...",
+        "I am a model enthusiast who did a PhD in modeling lake phytoplankton communities "
+        "- the tiny, drifting organism that is very important in ecosystems. While the model "
+        "itself is complex, the output can be distilled into insightful patterns by reducing "
+        "dimensionality. I was fascinated not just by the model but also the stories the data told.",
         styles={""}
         )
-st.sidebar.write("")
-st.sidebar.write(
-    ""
-)
-st.sidebar.write(
-    ""
-)
-# if st.sidebar.button("Connect with Debbie! ☕️"):
-#     st.sidebar.markdown(
-#         'https://www.linkedin.com/in/debbieszewingto/', unsafe_allow_html=True
-#     )
+# connect
 st.sidebar.link_button("Connect with Debbie ☕️", 
                        "https://www.linkedin.com/in/debbieszewingto/")
+# email
+if st.sidebar.button("Email Debbie  📧"):
+   st.markdown('<a href="mailto:toszewingdebbie@gmail.com">Click here if nothing opens</a>', unsafe_allow_html=True)
 
 st.sidebar.markdown("---")  # horizontal line
 # st.sidebar.selectbox("Go to", ["Home", "Analysis", "Settings"])
-st.sidebar.segmented_control("Useful links 🔗", ["Data Storage", "Code Documentation", "Settings"])
+# st.sidebar.segmented_control("Useful links 🔗", ["Data Storage", "Code Documentation", "Settings"])
 
-
+st.sidebar.markdown("## Useful links 🔗")
+st.sidebar.markdown("[Data Repository](https://zenodo.org/search?q=metadata.creators.person_or_org.name%3A%22To%2C%20Sze%20Wing%22&l=list&p=1&s=10&sort=bestmatch)")
+st.sidebar.markdown("[Code Documentation](https://github.com/Debbcwing)")
+# st.sidebar.markdown("[Settings](https://your-settings-link.com)")
 
 # ---------------------- HOME PAGE ----------------------
 if selected == sidebar_items[0]:
-    st.header("Home")
-    # st.subheader("Overview")
+    st.header("Welcome!")
+    # st.subheader("Welcome!")
     st.write(
-        ""
+        "Here you can submit any questions you have about my PhD research:"
     )
 
 
 
 # ---------------------- Data ----------------------
 if selected == sidebar_items[1]:
-    st.header("Data")
+    st.header("Data 📊📈")
     # st.subheader("Overview")
     st.write(
-        ""
+        "Have a look at the real lake data collected between years 2019 and 2022 by the state-of-art "
+        "underwater microscope placed at Greifensee, Switzerland🇨🇭"
     )
     tab_names_data = ["Physical", "Chemical", "Biological"]
     tab1, tab2, tab3 = st.tabs(tab_names_data)
+
+    with tab1:
+        st.write(
+            ""
+        )
+
+    with tab2:
+        st.write(
+            ""
+        )
+
+    with tab3:
+        st.write(
+            ""
+        )
 
 
 
@@ -111,8 +128,27 @@ if selected == sidebar_items[2]:
     tab_names_model = ["Concepts", "Baseline", "Reaction", "Forecast"]
     tab1, tab2, tab3, tab4 = st.tabs(tab_names_model)
 
+    with tab1:
+        st.subheader(
+            "A simplified lake ecosystem"
+        )
+        # @st.cache_data
+        st.image("Fig1.1_v2.png")
+    
+    with tab2:
+        st.write(
+            ""
+        )
 
-
+    with tab3:
+        st.write(
+            ""
+        )
+    
+    with tab4:
+        st.write(
+            ""
+        )    
 
 # ---------------------- Manuscript ----------------------
 if selected == sidebar_items[3]:
@@ -123,3 +159,30 @@ if selected == sidebar_items[3]:
     )
     tab_names_ms = ["Manuscript 1", "Manuscript 2", "Manuscript 3"]
     tab1, tab2, tab3 = st.tabs(tab_names_ms)
+
+
+
+# ---------------------- Planktoomics ----------------------
+if selected == sidebar_items[4]:
+    st.header("Stories of phytoplankton")
+    # @st.cache_data
+    st.image("StoryIntro.png")
+    # st.subheader("**Go to**")
+    if st.checkbox("The habitat of phytoplankton"):
+        st.image("Phyto_1.png")
+    if st.checkbox("Algae bloom under lake ice"):
+        st.image("Phyto_2.png")
+    if st.checkbox("The life of the aquatic photosynthesis machine"):
+        st.image("Phyto_3.png")
+    # st.selectbox("**Go to**", 
+    #             ["The habitat of phytoplankton", 
+    #              "Algae bloom under lake ice", 
+    #              "The life of the aquatic photosynthesis machine"])
+    # if "The habitat of phytoplankton":
+    #     st.image("Phyto_1.png")
+    # if "Algae bloom under lake ice":
+    #     st.image("Phyto_2.png")
+    # if "The life of the aquatic photosynthesis machine":
+    #     st.image("Phyto_3.png")
+    # tab_names_ms = ["Manuscript 1", "Manuscript 2", "Manuscript 3"]
+    # tab1, tab2, tab3 = st.tabs(tab_names_ms)
